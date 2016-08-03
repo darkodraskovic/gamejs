9414d33242a0bb276b9e849f005968b0348fa3ab
Darko Draskovic <darko.draskovic@gmail.com>
2016-08-03 08:49:00 +0200

    finished requirejs integration

---
+++ build.js
@@ -0,0 +1,27 @@
+({
+    baseUrl: ".",
+    
+    paths: {
+        "PIXI": "lib/pixi",
+        "SAT": "lib/SAT",
+        "Quadtree": "lib/quadtree",
+        "requireLib": "lib/require"
+    },
+
+    shim: {
+        "SAT": {
+            exports: 'SAT'
+        },
+        "Quadtree": {
+            exports: 'Quadtree'
+        }
+    },
+
+    name: "main",
+    out: "game.js",
+
+    // namespace: 'Game',
+    
+    optimize: "none",
+    include: ["requireLib"]
+})
--- main.js
+++ main.js
@@ -1,12 +1,75 @@
 requirejs(
-    ['core/application', 'core/scene', 'core/layer', 'core/entity', 'component/vehicle'],
-    function(Application, Scene, Layer, Entity, Component) {
+    [
+        'core/class',
+        'core/system',
         
-        var app = new Application();
-        var scene = new Scene();
-        var layer = new Layer(scene, 'layer');
-        var entity = new Entity(layer, 0, 0, {'name': 'entity1'});
-        entity.collider = new Component(entity);
-        app.start(scene);
+        'core/application',
+        'core/camera',
+        'core/component',
+        'core/container',
+        'core/entity',
+        'core/input',
+        'core/layer',
+        'core/pool',
+        'core/scene',
+        'core/tiledManager',
+        'core/tile',
+        'core/tilemap',
         
+        'component/animator',
+        'component/collider',
+        'component/kinematics',
+        'component/platformer',
+        'component/projectile',
+        'component/vehicle',
+    ],
+    function(
+        Class,
+        sys,
+        
+        Application,
+        Camera,
+        Component,
+        Container,
+        Entity,
+        Input,
+        Layer,
+        Pool,
+        Scene,
+        TiledManager,
+        Tile,
+        Tilemap,
+        
+        Animator,
+        Collider,
+        Kinematics,
+        Platformer,
+        Projectile,
+        Vehicle
+    ) {
+        Game = {};
+        
+        Game.Class = Class;
+        Game.sys = sys;
+        
+        Game.Application = Application;
+        Game.Camera = Camera;
+        Game.Component = Component;
+        Game.Container = Container;
+        Game.Entity = Entity;
+        Game.Input = Input;
+        Game.Layer = Layer;
+        Game.Pool = Pool;
+        Game.Scene = Scene;
+        Game.TiledManager = TiledManager;
+        Game.Tile = Tile;
+        Game.Tilemap = Tilemap;
+
+        Game.components = {};
+        Game.components.Animator = Animator;
+        Game.components.Collider = Collider;
+        Game.components.Kinematics = Kinematics;
+        Game.components.Platformer = Platformer;
+        Game.components.Projectile = Projectile;
+        Game.components.Vehicle = Vehicle;        
     });
